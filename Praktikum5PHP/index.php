0c3b7e148243d5cbc92528f7706bbd0d01083852
Daud334 <daudlairaoktavianusdaudno@gmail.com>
2026-03-11 17:29:45 +0700

    Praktikum_5_PHP

--- Praktikum5PHP/index.php
+++ Praktikum5PHP/index.php
@@ -2,15 +2,16 @@
 $namabarang = ["Memori", "Komputer", "Keyboard", "Handphone", "Mouse"];
 $hargasatuan = [550, 500, 150, 200, 50];
 $jumlahbeli = 2;
-$pajak = 0.1;
 $totalharga = $jumlahbeli * $hargasatuan[2];
+define("PAJAK", 0.1);
+$pajak = $totalharga * PAJAK ;
 
 echo "<h2><b>Perhitungan Total Pembelian Barang (Dengan Array)</b> </h2>";
-echo "<hr style='width:40%; border:2px solid black; margin-left:0;'>";
+echo "<hr>";
 echo "Nama Barang : " . $namabarang[2] . "<br>";
 echo "Harga Satuan : Rp " . $hargasatuan[2] . ".000" . "<br>";
 echo "Jumlah Beli : " . $jumlahbeli . "<br>";
 echo "Total Harga (Sebelum Pajak) : Rp " . $totalharga . ".000" . "<br>";
-echo "Pajak : Rp " . $pajak * $totalharga . ".000" . "<br>";
-echo "<b>Total Bayar : RP </b>" . $totalharga + $totalharga * $pajak . "<b>.000</b>" . "<br>";
+echo "Pajak : Rp " . $pajak . ".000" . "<br>";
+echo "<b>Total Bayar : RP </b>" . $totalharga +  $pajak . "<b>.000</b>" . "<br>";
 ?>
